9f7815abc95eecdafe3c48e2e4b0e88e86204d7a
MykolaSuprun <mykola.surpun@protonmail.com>
2023-07-20 04:46:46 +0200

    create neovim flake.nix13
    create flake.nix

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Neovim flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    neovim = {
+      url = "github:neovim/neovim/stable?dir=contrib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, neovim }: {
+      packages.x86_64-linux.default = neovim.packages.x86_64-linux.neovim;
+      apps.x86_64-linux.default = {
+        type = "app";
+        program = "${neovim.packages.x86_64-linux.neovim}/bin/nvim";
+      };
+    };
+}
